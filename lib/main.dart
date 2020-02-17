3b7dc8fb2bf62d51f1b84651904025090ff0864b
Brett New <jumpingneptune@gmail.com>
2020-02-17 17:51:51 -0600

    Create initial app with cupertino tab bar

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            title: Text('BMI'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings_solid),
+            title: Text('Settings'),
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int i) {
+        return Container();
+      },
+    );
+  }
+}
